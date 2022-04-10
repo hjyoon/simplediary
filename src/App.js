3699666fa7191e4085b7446aabe91d96284c0773
hjyoon <se1620236@naver.com>
2022-04-10 20:59:05 +0900

    Add component DiaryList and dummyList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "윤효전",
+    content: "하이",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "홍길동",
+    content: "하이 2",
+    emotion: 4,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "아무개",
+    content: "하이 3",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
