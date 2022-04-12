7c00327323a614972f7dc05310b4a28135401007
hjyoon <se1620236@naver.com>
2022-04-12 20:03:40 +0900

    Add editing item button and Rename onDelete to onRemove

--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,13 +1,18 @@
 import DiaryItem from "./DiaryItem";
 
-const DiaryList = ({ onDelete, diaryList }) => {
+const DiaryList = ({ onEdit, onRemove, diaryList }) => {
   return (
     <div className="DiaryList">
       <h2>일기 리스트</h2>
       <h4>{diaryList.length}개의 일기가 있습니다.</h4>
       <div>
         {diaryList.map((item) => (
-          <DiaryItem key={item.id} {...item} onDelete={onDelete} />
+          <DiaryItem
+            key={item.id}
+            {...item}
+            onEdit={onEdit}
+            onRemove={onRemove}
+          />
         ))}
       </div>
     </div>
